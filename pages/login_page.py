3ae7d7ac88ca9db602e3f180aca0c676a4dc1fdb
Svetlana Z <szakharova86@gmail.com>
2025-08-19 21:28:41 +0300

    Lesson_4_2_step9

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,15 +11,10 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         cururl=self.browser.current_url
         assert "login" in cururl, "Login is not found in url"
-        # реализуйте проверку на корректный url адрес
-        #assert True
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),"Login form is not presented"
-        #assert True
+
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM),"Register form is not presented"
-        #assert True
