6ae7843e0613b2a0f593b2f3e747981d19a621e0
Svetlana Z <szakharova86@gmail.com>
2025-08-20 15:51:05 +0300

    Lesson_4_3_step5

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,6 +2,10 @@ from .base_page import BasePage
 from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_IN_BASKET), \
+            "Success message is presented, but should not be"
+
     def add_to_basket(self):
         add_to_basket_btn=self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_to_basket_btn.click()
@@ -15,6 +19,10 @@ class ProductPage(BasePage):
         product_price=self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         product_price_in_basket=self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET).text
         assert product_price==product_price_in_basket, f"different products prices in product:{product_price} and basket:{product_price_in_basket}"
+
+    def is_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_IN_BASKET), \
+            "Message is presented, but should disappear"
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,7 @@ def test_guest_can_add_product_to_basket(browser, offernumber):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offernumber}"
     page = ProductPage(browser, link)
     page.open()
+    page.should_not_be_success_message()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
 
